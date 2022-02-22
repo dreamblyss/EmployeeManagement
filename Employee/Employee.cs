eb2cfdd5155532b20457a7081781dd28274fd31d
kuntzcode <iwekuba.henry2012@gmail.com>
2022-02-22 23:30:32 +0100

    Add project files.

---
+++ Employee/Employee.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Employee
+{
+    class Employee
+    {
+        //staff id - ro, fullname - ro, job title, employment date - ro, department
+
+        private int _staffId;
+        private string _fullName;
+        private string _employDate;
+
+        public Employee(int staffId, string fullName, string employDate)
+        {
+            _staffId = staffId;
+            _fullName = fullName;
+            _employDate = employDate;
+        }
+
+        public int StaffId { get { return _staffId; } }
+        public string FullName { get { return _fullName; } }
+
+        public string EmployDate { get { return _employDate; } }
+
+        public string JobTitle { get; set; }
+
+        public string Department { get; set; }
+
+        //clockin(), clockout(), work()
+
+        public DateTime ClockIn()
+        {
+            string clockIn = "";
+
+            DateTime clockintime = default(DateTime);
+
+            while (String.IsNullOrEmpty(clockIn))
+            {
+                Console.WriteLine("Enter \"Y\" to clockin: ");
+                string click = Console.ReadLine().ToUpper();
+
+                if (click == "Y")
+                {
+                    clockintime = DateTime.Now;
+                    //clockIn = clockintime.ToString("t");
+                }
+                else
+                {
+
+                    Console.WriteLine("Enter \"Y\" to Clock In");
+                }
+            }
+
+
+            return clockintime;
+        }
+
+
+        public DateTime ClockOut()
+        {
+
+
+            string clockOut = "";
+
+            DateTime clockoutime = default(DateTime);
+
+            while (String.IsNullOrEmpty(clockOut))
+            {
+                Console.WriteLine("Enter \"Y\" to clockout: ");
+                string click = Console.ReadLine().ToUpper();
+
+                if (click == "Y")
+                {
+                    clockoutime = DateTime.Now;
+                    //clockIn = clockintime.ToString("t");
+                }
+                else
+                {
+
+                    Console.WriteLine("Enter \"Y\" to Clock Out");
+                }
+            }
+
+            return clockoutime;
+
+        }
+
+        public void Work()
+        {
+
+            DateTime timeIn = ClockIn();
+            DateTime timeOut = ClockOut();
+
+            TimeSpan workDuration = TimeSpan.Zero;
+            workDuration = timeOut.Subtract(timeIn.Date);
+            string hours = workDuration.Hours.ToString();
+            string minutes = workDuration.Minutes.ToString();
+
+            Console.WriteLine($"You worked for {hours} hours and {minutes} minutes");
+
+        }
+
+        public override string ToString()
+        {
+
+            return this.StaffId + " " + this.FullName + " " + this.JobTitle + " " +
+                this.Department + " " + this.JobTitle;
+
+
+        }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//using EmployeeManagement.Staff;
+//using EmployeeManagement.Department;
+
+using EmployeeManagement.Employee;
+using EmployeeManagement.Department;
+using System.Collections;
+
+static String ObtainDateStamp(DateTime value)
+{
+    return value.ToString("yyyyMMdd");
+}
+
+Department db1 = new Department();
+
+String timeStamp = ObtainDateStamp(DateTime.Now);
+
+db1.Departments = new string[] { "research", "sales", "accounting", "finance", "engineering", "admin"};
+
+db1.JobTitles = new string[] { "Engineer", "Sales Executive", "Program Manager",
+    "Product Manager", "Accountant", "Auditor", "Manager", "Senior Manger", "GM", "MD", "CTO", "ED",
+"executive", "Clerk", "Technician"};
+
+//String timeStamp = GetTimestamp(DateTime.Now); 
+
+//staf id, name and date of employment
+Employee employee1 = new Employee(100, "Grace Alika", timeStamp);
+Employee employee2 = new Employee(101, "Musa Badru",  timeStamp);
+Employee employee3 = new Employee(102, "Ogbonna Nkamuke", timeStamp);
+Employee employee4 = new Employee(103, "Obi Uma Akapbio", timeStamp);
+Employee employee5 = new Employee(104, "Augustine Sola", timeStamp);
+//department
+employee1.Department = db1.Departments[4];
+employee2.Department = db1.Departments[1];
+employee3.Department = db1.Departments[4];
+employee4.Department = db1.Departments[4];
+employee5.Department = db1.Departments[5];
+
+//job title
+employee1.JobTitle = db1.JobTitles[0];
+employee2.JobTitle = db1.JobTitles[1];
+employee3.JobTitle = db1.JobTitles[2];
+employee4.JobTitle = db1.JobTitles[3];
+employee5.JobTitle = db1.JobTitles[4];
+//Store all 5 employees in an array of employees
+
+ArrayList employees = new ArrayList();
+
+employees.Add(employee1);
+employees.Add(employee2);
+employees.Add(employee3);
+employees.Add(employee4);
+employees.Add(employee5);
+
+
+foreach (object obj in employees)
+{   
+    Console.WriteLine(obj);
+}
+
+
+
+
